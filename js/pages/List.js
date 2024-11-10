819fd96a00002b24fc58e1c92216ac78fc386a93
TheFluxite <fluxitegmd@gmail.com>
2024-11-10 18:04:23 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,25 +100,31 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (for example: macros or /tick)
+                        The Level should be focused on Nerve Control if you decide to verify it. You can submit your verification by contacting "thefluxite" on Discord
                     </p>
                     <p>
-                        Achieved the record on the Map that is listed on the site
+                        Achieved the record without using hacks (however, FPS bypass and CBF is allowed, up to 360fps)
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a second of the previous attempt shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
                     <p>
-                        The recording must also show the player completing the cource completely with 1 minute after the completion included, or the completion will be invalidated.
+                        The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes or exploits
+                        Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Once a Map falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
                     </p>
                 </div>
             </div>
